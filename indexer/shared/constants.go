d82ea169fb709c488031d1a1744db3d22e2207c3
Marko Boben <marko.boben@gmail.com>
2025-06-16 13:09:48 +0200

    Update version, Durango fork times

--- indexer/shared/constants.go
+++ indexer/shared/constants.go
@@ -5,16 +5,16 @@ import (
 )
 
 const (
-	ApplicationVersion = "2.1.0"
+	ApplicationVersion = "2.2.0"
 )
 
 var (
 	// Map from network name (HRP) to Durango fork time
 	DurangoTimes = map[string]time.Time{
 		"flare":      time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
-		"costwo":     time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
+		"costwo":     time.Date(2025, time.June, 24, 12, 0, 0, 0, time.UTC),
 		"localflare": time.Date(2025, time.May, 15, 14, 0, 0, 0, time.UTC),
-		"coston":     time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
+		"coston":     time.Date(2025, time.July, 1, 12, 0, 0, 0, time.UTC),
 		"songbird":   time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
 		"local":      time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
 	}
