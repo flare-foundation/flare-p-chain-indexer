c815747a8b7ef6e090bd57ea2357534f4c65b42d
Marko Boben <marko.boben@gmail.com>
2025-07-09 11:27:16 +0200

    set Durango times for Songbird and Flare

--- indexer/shared/constants.go
+++ indexer/shared/constants.go
@@ -11,11 +11,11 @@ const (
 var (
 	// Map from network name (HRP) to Durango fork time
 	DurangoTimes = map[string]time.Time{
-		"flare":      time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
+		"flare":      time.Date(2025, time.August, 5, 12, 0, 0, 0, time.UTC),
 		"costwo":     time.Date(2025, time.June, 24, 12, 0, 0, 0, time.UTC),
-		"localflare": time.Date(2025, time.May, 15, 14, 0, 0, 0, time.UTC),
+		"localflare": time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
 		"coston":     time.Date(2025, time.July, 1, 12, 0, 0, 0, time.UTC),
-		"songbird":   time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
-		"local":      time.Date(10000, time.December, 1, 0, 0, 0, 0, time.UTC),
+		"songbird":   time.Date(2025, time.July, 22, 12, 0, 0, 0, time.UTC),
+		"local":      time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
 	}
 )
