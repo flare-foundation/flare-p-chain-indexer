d69e003c46b1da8956e94b2fcd40bb5232462e5e
Marko Boben <marko.boben@gmail.com>
2025-08-07 13:48:35 +0200

    Fix parsing getTx response

--- indexer/pchain/utils.go
+++ indexer/pchain/utils.go
@@ -1,14 +1,16 @@
 package pchain
 
 import (
+	"encoding/json"
 	"flare-indexer/utils/chain"
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/formatting"
-	"github.com/ava-labs/avalanchego/utils/json"
+	avaJson "github.com/ava-labs/avalanchego/utils/json"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
 	"github.com/ava-labs/avalanchego/vms/platformvm/genesis"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
+	"github.com/pkg/errors"
 )
 
 func CallPChainGetTxApi(client chain.RPCClient, txID string) (*txs.Tx, error) {
@@ -29,7 +31,12 @@ func CallPChainGetTxApi(client chain.RPCClient, txID string) (*txs.Tx, error) {
 	}
 
 	// Parse from hex string
-	txData, err := formatting.Decode(formatting.Hex, string(reply.Tx))
+	var txString string
+	err = json.Unmarshal(reply.Tx, &txString)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal tx string")
+	}
+	txData, err := formatting.Decode(formatting.Hex, txString)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +53,7 @@ func CallPChainGetTxApi(client chain.RPCClient, txID string) (*txs.Tx, error) {
 //
 // To avoid an additional dependency
 type GetRewardUTXOsReply struct {
-	NumFetched json.Uint64         `json:"numFetched"`
+	NumFetched avaJson.Uint64      `json:"numFetched"`
 	UTXOs      []string            `json:"utxos"`
 	Encoding   formatting.Encoding `json:"encoding"`
 }
